6b748c417296058353e95e3bc5e9dd2fa0ccf77a
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2020-12-11 09:46:44 -0500

    Do not make checksums of checksums

--- make_check_sums.py
+++ make_check_sums.py
@@ -6,13 +6,17 @@ from xarray.tutorial import file_md5_checksum
 
 
 def main():
-    files = [f for f in Path().cwd().rglob("*") if f.parent.name != "raven-testdata"]
+    files = [
+        f
+        for f in Path().cwd().rglob("*")
+        if f.parent.name != "raven-testdata" and not str(f).endswith(".md5")
+    ]
     for f in files:
-        outf = f'{f.as_posix()}.md5'
+        outf = f"{f.as_posix()}.md5"
         if f.is_file() and not Path(outf).exists():
-            with open(outf, 'w') as out:
+            with open(outf, "w") as out:
                 out.write(file_md5_checksum(f))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
